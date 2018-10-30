a157e9fdf7adf6e6c265dc5d0184e787e52e0afc
wallandall <wallandall@gmail.com>
2018-10-30 23:17:12 +0100

    Setup Express and configure server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('/models/todo');
-var {User} = require('/models/user');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.post('/todos',(req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
