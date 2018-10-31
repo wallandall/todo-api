7ad2c8dc96ed3fc023a5c8623b5bad28121ef4d9
wallandall <wallandall@gmail.com>
2018-10-31 09:48:03 +0100

    Added Get /todo route

--- server/server.js
+++ server/server.js
@@ -19,6 +19,14 @@ app.post('/todos',(req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
