ea9a5cbc939a89cec4507a67561e574e29ad700c
wallandall <wallandall@gmail.com>
2018-11-03 19:16:49 +0100

    Update config to use JSON File

--- README.md
+++ README.md
@@ -44,6 +44,8 @@ npm install
   - Defines the URL for MongoDB
 - NODE_ENV
   - The Node Environment eg: production, test or development
+- JWT_SECRET
+  - The Secret Key used to generate tokens
 
 ## To to run the program type the below command from the application directory
 
--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,20 @@
 var env = process.env.NODE_ENV || 'development';
-if (env === 'development') {
-  process.env.PORT = 8000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp",{ useNewUrlParser: true }
-} else if(env === 'test'){
-  process.env.PORT = 8000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest",{ useNewUrlParser: true }
+
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+
+
+// if (env === 'development') {
+//   process.env.PORT = 8000;
+//   process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp",{ useNewUrlParser: true }
+// } else if(env === 'test'){
+//   process.env.PORT = 8000;
+//   process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest",{ useNewUrlParser: true }
+// }
